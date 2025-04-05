6011bdb8079b8bc8c3a3f1249d6079191f6832f2
Jean García <116606354+Y34NxD@users.noreply.github.com>
2025-04-05 13:37:37 -0600

    Add files via upload

---
+++ Practica7/Window.cpp
@@ -0,0 +1,210 @@
+#include "Window.h"
+
+Window::Window()
+{
+	width = 800;
+	height = 600;
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+Window::Window(GLint windowWidth, GLint windowHeight)
+{
+	width = windowWidth;
+	height = windowHeight;
+	muevex = 0.0f;
+	rotaLlantas = 0.0f;
+	rotaCofre = 0.0f;
+	TransladaCarro = 0.0f;
+
+	for (size_t i = 0; i < 1024; i++)
+	{
+		keys[i] = 0;
+	}
+}
+int Window::Initialise()
+{
+	//Inicialización de GLFW
+	if (!glfwInit())
+	{
+		printf("Falló inicializar GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Asignando variables de GLFW y propiedades de ventana
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	//para solo usar el core profile de OpenGL y no tener retrocompatibilidad
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+	//CREAR VENTANA
+	mainWindow = glfwCreateWindow(width, height, "Practica07:Iluminación 1", NULL, NULL);
+
+	if (!mainWindow)
+	{
+		printf("Fallo en crearse la ventana con GLFW");
+		glfwTerminate();
+		return 1;
+	}
+	//Obtener tamaño de Buffer
+	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+
+	//asignar el contexto
+	glfwMakeContextCurrent(mainWindow);
+
+	//MANEJAR TECLADO y MOUSE
+	createCallbacks();
+
+
+	//permitir nuevas extensiones
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK)
+	{
+		printf("Falló inicialización de GLEW");
+		glfwDestroyWindow(mainWindow);
+		glfwTerminate();
+		return 1;
+	}
+
+	glEnable(GL_DEPTH_TEST); //HABILITAR BUFFER DE PROFUNDIDAD
+							 // Asignar valores de la ventana y coordenadas
+							 
+							 //Asignar Viewport
+	glViewport(0, 0, bufferWidth, bufferHeight);
+	//Callback para detectar que se está usando la ventana
+	glfwSetWindowUserPointer(mainWindow, this);
+}
+
+void Window::createCallbacks()
+{
+	glfwSetKeyCallback(mainWindow, ManejaTeclado);
+	glfwSetCursorPosCallback(mainWindow, ManejaMouse);
+}
+GLfloat Window::getXChange()
+{
+	GLfloat theChange = xChange;
+	xChange = 0.0f;
+	return theChange;
+}
+
+GLfloat Window::getYChange()
+{
+	GLfloat theChange = yChange;
+	yChange = 0.0f;
+	return theChange;
+}
+
+
+
+
+void Window::ManejaTeclado(GLFWwindow* window, int key, int code, int action, int mode)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(window, GL_TRUE);
+	}
+	if (key == GLFW_KEY_Y)
+	{
+		theWindow-> muevex += 1.0;
+	}
+	if (key == GLFW_KEY_U)
+	{
+		theWindow-> muevex -= 1.0;
+	}
+
+	//Rotación para las llantas
+	if (key == GLFW_KEY_Z)
+	{
+		theWindow->rotaLlantas += 10.0; //rotar sobre el eje z 10 grados
+
+	}
+	if (key == GLFW_KEY_X)
+	{
+		theWindow->rotaLlantas -= 10.0; //rotar sobre el eje z 10 grados
+
+	}
+	//Rotación para el cofre
+	if (key == GLFW_KEY_V)
+	{
+		if (theWindow->rotaCofre < -45)
+		{
+		}
+		else
+		{
+			theWindow->rotaCofre -= 10.0; //rotar sobre el eje z 10 grados
+		}
+
+	}
+	if (key == GLFW_KEY_B)
+	{
+		if (theWindow->rotaCofre == 0.0)
+		{
+		}
+		else
+		{
+			theWindow->rotaCofre += 10.0; //rotar sobre el eje z 10 grados
+		}
+
+	}
+
+	//Transladar el carro
+	if (key == GLFW_KEY_1)
+	{
+		theWindow->TransladaCarro -= 0.5; //traslada sobre el eje x 
+		theWindow->rotaLlantas += 10.0; //Rota las llantas de acuerdo al sentido al que avanza el carro
+
+	}
+	if (key == GLFW_KEY_2)
+	{
+		theWindow->TransladaCarro += 0.5; //traslada sobre el eje x 
+		theWindow->rotaLlantas -= 10.0; //Rota las llantas de acuerdo al sentido al que avanza el carro
+	}
+
+	
+
+
+	if (key >= 0 && key < 1024)
+	{
+		if (action == GLFW_PRESS)
+		{
+			theWindow->keys[key] = true;
+			//printf("se presiono la tecla %d'\n", key);
+		}
+		else if (action == GLFW_RELEASE)
+		{
+			theWindow->keys[key] = false;
+			//printf("se solto la tecla %d'\n", key);
+		}
+	}
+}
+
+void Window::ManejaMouse(GLFWwindow* window, double xPos, double yPos)
+{
+	Window* theWindow = static_cast<Window*>(glfwGetWindowUserPointer(window));
+
+	if (theWindow->mouseFirstMoved)
+	{
+		theWindow->lastX = xPos;
+		theWindow->lastY = yPos;
+		theWindow->mouseFirstMoved = false;
+	}
+
+	theWindow->xChange = xPos - theWindow->lastX;
+	theWindow->yChange = theWindow->lastY - yPos;
+
+	theWindow->lastX = xPos;
+	theWindow->lastY = yPos;
+}
+
+
+Window::~Window()
+{
+	glfwDestroyWindow(mainWindow);
+	glfwTerminate();
+
+}
